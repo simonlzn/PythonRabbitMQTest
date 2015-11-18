cc69425df7542e21d69a859333d59817bd6bb9c9
Zinan Lin <simonlininter@yahoo.com>
2015-11-18 16:56:55 +0800

    Modified the receive file

--- recv.py
+++ recv.py
@@ -1,22 +1,23 @@
 import pika
+import time
 from send import *
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
 
-
-channel.queue_declare(queue='queue2')
+channel.queue_declare(queue='queue3')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    time.sleep(5)
     sender = Sender()
     sender.send()
 
 channel.basic_consume(callback,
-                      queue='queue2',
+                      queue='queue3',
                       no_ack=True)
 
 channel.start_consuming()
